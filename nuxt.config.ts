61539e4a8e89d4bb9834f2db80ec182d65688f28
marhel28 <ammarmaulahela20@gmail.com>
2026-07-17 18:40:46 +0700

    uploads

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000'
+      apiBase: process.env.VITE_API_BASE || process.env.NUXT_PUBLIC_API_BASE || 'https://cctv-api.desa-sidomukti.com'
     }
   },
 
